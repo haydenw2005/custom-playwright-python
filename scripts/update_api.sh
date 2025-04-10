2eda343863c08fb0c7f1b510606de0dfd92aeecc
haydenw2005 <haydenswhite@gmail.com>
2025-04-10 14:16:36 -0400

    updated playwright script

--- scripts/update_api.sh
+++ scripts/update_api.sh
@@ -16,9 +16,9 @@ function update_api {
     fi
 }
 
-update_api "playwright/sync_api/_generated.py" "scripts/generate_sync_api.py"
-update_api "playwright/async_api/_generated.py" "scripts/generate_async_api.py"
+update_api "custom_playwright/sync_api/_generated.py" "scripts/generate_sync_api.py"
+update_api "custom_playwright/async_api/_generated.py" "scripts/generate_async_api.py"
 
-playwright install
+custom_playwright install 
 
 python scripts/update_versions.py
